ec1e9a6baeb118084f128d56d9bd2ba79048d3e1
mostro <glhryonosuke@gmail.com>
2020-10-26 12:32:21 -0600

    Login con BD

--- php/usr/USRvalida.php
+++ php/usr/USRvalida.php
@@ -9,7 +9,6 @@ else{
 	$psw="";
 }
 $usr=strtolower($usr);// todos los usuarios se escriben en minusculas
-
 /************** conectamos a la BD**************/
 $serverdb = "localhost";
 $usrdb = "xito";
@@ -19,53 +18,32 @@ $db="tiendita";
 $cnxdb= new mysqli($serverdb, $usrdb, $pswdb,$db);//Conexion Base de datos
 if($cnxdb->connect_error){die("No se ha establecido la Conexion"); exit();}
 
-
+/** Seleccionamos la BD ************************************ */
 $cnxdb->select_db("tiendita");
-/*******************************************************************/
 
-$sql = "SELECT * FROM usuarios WHERE id=".$usr;
+/*** Consultamos el id y psw ********************************* */
+$sql = "SELECT id, psw FROM usuarios WHERE id='".$usr."'";
 $resultado=$cnxdb->query($sql);
-if (!$resultado) {echo "ERROR";} 
+$fila=$resultado->fetch_array(MYSQLI_BOTH);
 
-
-$fila=$resultado->fetch_array(MYSQLI_NUM);
-printf ("%s (%s)\n", $fila[0], $fila[1]);
-
-/***Comparamos con las variables ******************************************************* */
-if($fila['id']==$usr&&$fila['psw']==$psw){
-	session_start();
-	$_SESSION['activo']=true;
-	$_SESSION['usr']=$usr;
-	header("Location:../prueba.php");
-	/*
-	$_SESSION['idkrea']=$fila['idkrea'];// Si existe asignamos variables encontradas
-	$usrbd=$_SESSION['usuario']=$fila['usuario'];
-	$pswbd=$fila['password'];	//$_SESSION['password']=$fila['password'];
-	$status=$fila['status'];			//$_SESSION['status']=$fila['status'];
-	$_SESSION['permisos']=$fila['permisos'];
-	*/
-	}
-else{
-	//header("Location:../../index.php?id=".$fila['id']."&psw=".$fila['psw']);
-	//header("Location:../../index.php?mensaje=2");
-}
-echo "id=".$fila['id']."  psw=".$fila['psw'];
-
-$resultado->free();
-$cnxdb->close();//cerramos la base de datos
-	
-/************************************************************************************
-
-if($usr=="xito" && $psw=="123"){
-	session_start();
-	$_SESSION['activo']=true;
-	$_SESSION['usr']=$usr;
-	header("Location:../prueba.php");
-}
-else{
+/** Verificamos si Corresponde ****************************** */
+if (empty($fila)){//No existe coincidencia con id
 	header("Location:../../index.php?mensaje=2");
 }
+else{//Existe Coincidencia
+	if($fila['id']==$usr&&$fila['psw']==$psw){//Verifica que coincidan ambos atributos para iniciar sesión y permitir accesos
+		session_start();
+		$_SESSION['activo']=true;
+		$_SESSION['usr']=$usr;
+		header("Location:../prueba.php");
+		}
+	else{//Si no coincide alguno regresamos
+		header("Location:../../index.php?mensaje=2");
+	}
+} 
+/** Liberamos resultado y cerramos BD *************************  */
+$resultado->free();
+$cnxdb->close();//cerramos la base de datos
 
-/********************************************************************************************/
-
+/*****************************************************************/
 ?>
