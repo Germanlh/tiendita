6e9e4d14d638b15484c6b6f38793236cd82ef5c9
mostro <glhryonosuke@gmail.com>
2020-10-28 18:49:08 -0600

    prueba con index

--- index.php
+++ index.php
@@ -1,27 +1,49 @@
 <?php
 if(isset($_GET['op'])){
     $op=$_GET['op'];
+}
+else{
+    $op=1;
+}
     switch($op){
         case 1:
-            $link="";
+            $link="?op=2";
+            $atext="dos";
+            $page="1.php";
             break;
+        case 2:
+            $link="?op=3";
+            $atext="tres";
+            $page="2.php";
+            break;
+        case 3:
+            $link="?op=1";
+            $atext="uno";
+            $page="3.php";
+            break;
+
         default:
             break;
     }
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
     <title>TIENDITA</title>
 </head>
 <body>
 	<header>
-		<a href="?op=1"><?php echo $link ?></a>
+		<a href=<?php echo "'".$link."'"; ?>><?php echo $atext; ?></a>
 	</header>
+    <main>
+        <?php include $page;?>
 
-    
+    </main>
+    <footer>
+    </footer>    
 </body>
 </html>
