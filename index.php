1d717c113de746538f632bdbd62c96deb815f6a1
mostro-lap <glhryonosuke@gmail.com>
2020-11-28 20:38:40 -0600

    Modificando Login

--- index.php
+++ index.php
@@ -1,47 +1,28 @@
-<?php
-if(isset($_GET['op'])){
-    $op=$_GET['op'];
-}
-else{
-    $op=1;
-}
-    switch($op){
-        case 1:
-            $link="?op=2";
-            $atext="dos";
-            $page="1.php";
-            break;
-        case 2:
-            $link="?op=3";
-            $atext="tres";
-            $page="2.php";
-            break;
-        case 3:
-            $link="?op=1";
-            $atext="uno";
-            $page="3.php";
-            break;
-
-        default:
-            break;
-    }
-?>
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
+<html>
+  <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilos.css">
-    <title>TIENDITA</title>
-</head>
-<body>
-	<header>
-		<a href=<?php echo "'".$link."'"; ?>><?php echo $atext; ?></a>
-	</header>
-    <main>
-        <?php include $page;?>
-    </main>
-    <footer>
-    </footer>    
-</body>
+    <title>Tiendita AAA</title>
+    <link href="css/estilos.css" type="text/css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.svg" type="image/x-icon">
+  </head>
+  <body>
+
+    <div class="login-box">
+      <img src="img/logoAAA.svg" class="avatar" alt="Avatar Image">
+      <h1>Login Here</h1>
+      <form>
+        <!-- USERNAME INPUT -->
+        <label for="username">Username</label>
+        <input type="text" placeholder="Enter Username">
+        <!-- PASSWORD INPUT -->
+        <label for="password">Password</label>
+        <input type="password" placeholder="Enter Password">
+        <input type="submit" value="Log In">
+        <a href="#">Lost your Password?</a><br>
+        <a href="#">Don't have An account?</a>
+      </form>
+    </div>
+  </body>
 </html>
